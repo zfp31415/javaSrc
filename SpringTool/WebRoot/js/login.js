8aa88995096b812d0784a073c56c34daafa911cc
zfp31415 <zfp31415@126.com>
2016-04-11 20:49:00 +0800

    ajax Get Example

--- SpringTool/WebRoot/js/login.js
+++ SpringTool/WebRoot/js/login.js
@@ -8,9 +8,45 @@ function mclick(){
 
 //登陆的ajax
  function mLoad(name,pwd) {
+	 var xmlHttpReg= null;
+	 if(window.ActiveXObject)
+		 {
+		  xmlHttpReg = new ActiveXobject("Micrsoft.XMLHTTP");
+		 }
+	 else if(window.XMLHttpRequest)
+		 {
+		   xmlHttpReg = new XMLHttpRequest();
+		 }
+	 
+	 if(xmlHttpReg!=null)
+		 {
+		  xmlHttpReg.open('get', "1.txt", true, name, pwd);
+		  xmlHttpReg.send(null);
+		  xmlHttpReg.onreadystatechange = doResult;
+		 }
+	 else
+		 {
+		  alert("xmlHttpReg is null");
+		 }
 	//xm = XMLHttpRequest();
 	 //实现页面的跳转
-	 top.location.href="./index.jsp";
-	 alert("Loading..."+name+pwd);
+	 
+	 function doResult(){
+		 if(xmlHttpReg.readyState==4)
+			 {
+			 if(xmlHttpReg.status==200)
+				// document.getElementById("LoginPanel").innerHTML = xmlHttpReg.responseText;
+				 if(name ==xmlHttpReg.responseText)
+			        top.location.href="./index.jsp";
+				 else
+					 {
+					   alert("密码错误");
+					 }
+			 }
+
+	 }
+	
+	
 	// return false;
 }
+ 
